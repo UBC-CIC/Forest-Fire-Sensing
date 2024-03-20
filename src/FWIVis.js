6c94a99b33579234d2f3700fffa28a3aee08b4e1
Gurjot Sandhu <gsandh02@student.ubc.ca>
2024-03-20 13:09:48 -0700

    Moved from react-vis to mui/x-charts

--- src/FWIVis.js
+++ src/FWIVis.js
@@ -1,21 +1,34 @@
-import { XYPlot, LineSeries, VerticalGridLines, HorizontalGridLines, XAxis, YAxis } from "react-vis";
-import '../node_modules/react-vis/dist/style.css';
+import { LineChart } from "@mui/x-charts";
 
 function FWIVis(FWIData) {
-    const formattedData = [];
+    const timeData = [];
+    const indexData = [];
     FWIData = FWIData['FWIdata']['satellite'];
     FWIData.forEach((item) => {
-        formattedData.push({ x: item.timestamp*1000, y: item.fwi })
+        timeData.push(item.timestamp*1000);
+        indexData.push(item.fwi);
     });
 
     return (
-        <XYPlot height={300} width={300}>
-            <XAxis xType="time" title="Date" />
-            <YAxis title="Fire Weather Index (FWI)" />
-            <VerticalGridLines />
-            <HorizontalGridLines />
-            <LineSeries data={formattedData}></LineSeries>
-        </XYPlot>
+        <LineChart
+            width={300}
+            height={300}
+            xAxis={[
+                {
+                    id: 'Date',
+                    data: timeData,
+                    scaleType: 'time',
+                }
+            ]}
+            series={[
+                {
+                    id: 'FWI',
+                    label: 'Fire Weather Index',
+                    data: indexData
+                }
+
+            ]}
+        />
     );
 }
 

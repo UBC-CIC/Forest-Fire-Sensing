31b6e0f559a9d90bbec5780672a577ef53d165b4
Christy Zhang <gzhang16@student.ubc.ca>
2024-03-31 17:36:44 -0700

    updated deleteSensorData function

--- amplify/backend/function/deleteSensorData/src/index.js
+++ amplify/backend/function/deleteSensorData/src/index.js
@@ -4,6 +4,9 @@
 	STORAGE_SATELLITEDATA_ARN
 	STORAGE_SATELLITEDATA_NAME
 	STORAGE_SATELLITEDATA_STREAMARN
+	STORAGE_SENSORDATA_ARN
+	STORAGE_SENSORDATA_NAME
+	STORAGE_SENSORDATA_STREAMARN
 Amplify Params - DO NOT EDIT */
 
 /**
@@ -28,6 +31,7 @@ exports.handler = async (event) => {
 
     console.log(sensorID)
 
+    // delete satellite data
     const queryParams = {
         TableName: 'satelliteData-ampdev',
         KeyConditionExpression: 'sensorID = :sensorID',
@@ -53,6 +57,37 @@ exports.handler = async (event) => {
     try{
         const res = await ddb.query(queryParams).promise();
         await deleteItems(res.Items);
+    }catch(err){
+        return errorReturnVal(JSON.stringify(err));
+    }
+
+    // delete sensor data
+
+    const queryParams_sensor = {
+        TableName: 'sensorData-ampdev',
+        KeyConditionExpression: 'sensorID = :sensorID',
+        ExpressionAttributeValues: {
+            ':sensorID': sensorID
+        }
+    };
+
+    async function deleteItems_sensor(items) {
+        for (const item of items) {
+            const deleteParams = {
+                TableName: 'sensorData-ampdev',
+                Key: {
+                    'sensorID': item.sensorID,
+                    'timestamp': item.timestamp 
+                }
+            };
+            const deleteData = await ddb.delete(deleteParams).promise();
+            console.log("DeleteItem succeeded:", JSON.stringify(deleteData));
+        }
+    }
+
+    try{
+        const res_sensor = await ddb.query(queryParams_sensor).promise();
+        await deleteItems_sensor(res_sensor.Items);
     }catch(err){
         return errorReturnVal(JSON.stringify(err));
     }

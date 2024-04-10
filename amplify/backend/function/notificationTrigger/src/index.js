c031c787aac3f3c63d2189365527fffa6813392f
Gurjot Sandhu <gsandh02@student.ubc.ca>
2024-04-09 18:49:52 -0700

    Filter users for notifications based on location and public/private preferences

--- amplify/backend/function/notificationTrigger/src/index.js
+++ amplify/backend/function/notificationTrigger/src/index.js
@@ -14,35 +14,73 @@ AWS.config.update({
     region: 'ca-central-1'
 })
 
+function isWithinRegion(regionLat, regionLon, userLat, userLon){
+    var dLat = (regionLat - userLat) * Math.PI / 180;
+    var dLon = (regionLon - userLon) * Math.PI / 180;
+    var a = 0.5 - Math.cos(dLat) / 2 + Math.cos(userLat * Math.PI / 180) * Math.cos(regionLat * Math.PI / 180) * (1 - Math.cos(dLon)) / 2;
+    var distance = 6371000 * 2 * Math.asin(Math.sqrt(a));
+    console.log(distance);
+
+    return (distance <= 25000);
+}
+
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
+
+    var sensorID = event['queryStringParameters']['sensorID'];
 
     const params = {
         TableName: 'userSubLocations-ampdev',
         ProjectionExpression: 'username, lat, lon', 
     };
 
+    const deviceParams = {
+        TableName: 'sensors-ampdev',
+        FilterExpression: '#sensorID = :value',
+        ExpressionAttributeValues: {
+            ':value': {S: sensorID}
+        },
+        ExpressionAttributeNames: {
+            '#sensorID' : 'sensorID'
+        }
+    }
+
     try {
         const data = await dynamoDB.scan(params).promise();
+        const deviceData = await dynamoDB.scan(deviceParams).promise();
+        console.log(deviceData.Items);
+
+        if(deviceData.Items.length == 0){
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ message: 'The device\'s location is not defined, unable to send notifications to users' })
+            };
+        }
+
+        var deviceLat = deviceData.Items[0].lat.N;
+        var deviceLon = deviceData.Items[0].lon.N;
+        var isPublic = deviceData.Items[0].publicLocation.BOOL;
+        var deviceOwner = deviceData.Items[0].user.S;
+
         var userEmails = [];
         var userNumbers = [];
 
-        // Maybe switch over to storing email/phone numbers directly in dynamoDB if privacy/security concerns not an issue
         // Retrieve user attributes from cognito
-        // TODO: filter out users who are outside the region (currently adding everyone)
-        console.log(data);
         for(item of data.Items){
-            const user = await cognito.adminGetUser({
-                UserPoolId: process.env.COGNITO_USER_POOL_ID,
-                Username: item.username.S
-            }).promise();
-
-            user.UserAttributes.forEach(attribute => {
-                if(attribute.Name === 'email')
-                    userEmails.push(attribute.Value)
-                else if(attribute.Name === 'phone_number')
-                    userNumbers.push(attribute.Value)
-            });
+            console.log()
+            if(isWithinRegion(deviceLat, deviceLon, item.lat.N, item.lon.N) && (isPublic || (deviceOwner === item.username.S))){
+                const user = await cognito.adminGetUser({
+                    UserPoolId: process.env.COGNITO_USER_POOL_ID,
+                    Username: item.username.S
+                }).promise();
+    
+                user.UserAttributes.forEach(attribute => {
+                    if(attribute.Name === 'email' && !userEmails.includes(attribute.Value))
+                        userEmails.push(attribute.Value)
+                    else if(attribute.Name === 'phone_number' && !userNumbers.includes(attribute.Value))
+                        userNumbers.push(attribute.Value)
+                });
+            }
         }
 
         // Subscribe email and phone number to topic
